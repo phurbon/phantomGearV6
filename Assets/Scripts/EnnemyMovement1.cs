d9204164db72d350c934dcc476fa7d6bce8874d2
phurbon <pierreluchurbon@gmail.com>
2025-02-25 15:01:34 -0500

    ennemymovement

--- Assets/Scripts/EnnemyMovement1.cs
+++ Assets/Scripts/EnnemyMovement1.cs
@@ -7,16 +7,34 @@ public class EnnemyMovement1 : MonoBehaviour
 
     [SerializedField]
     private float _rotationSpeed;
-    private RigidBody2D _rigidbody
+    private RigidBody2D _rigidbody;
+    private PlayerAwarenessController _playerAwarenessController;
+    private Vector2 _target
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+    private oid Awake(){
+        _rigidbody = GetComponent<RigidBody2D>();
+        _playerAwarenessController = GetComponent<PlayerAwarenessController>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        UpdateTargetDirection();
+        RotateTowardsTarget();
+        SetVelocity();
+    }
+
+    private void UpdateTargetDirection(){
+
+
+    }
+
+    private void RotateTowardsTarget(){
+
+
+    }
+    private void SetVelocity()
+    {
+
     }
 }
